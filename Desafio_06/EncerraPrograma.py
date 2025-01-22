723536347ece9779f4ccfacbf82f2f1916d079b5
Luciano Moser <lucianomoser01@gmail.com>
2025-01-21 21:12:20 -0300

    Incluido o desafio 06

---
+++ Desafio_06/EncerraPrograma.py
@@ -0,0 +1,10 @@
+
+contador = 0
+valorInformado = int(input("Digite um número inteiro: "))
+while valorInformado != 10:
+    contador += 1
+    valorInformado = int(input("Informe um número inteiro: "))
+
+
+
+
