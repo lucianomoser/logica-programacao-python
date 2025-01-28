b449ee99bcc9b9a0cc8ad213af714f7529640182
Luciano Moser <lucianomoser01@gmail.com>
2025-01-27 23:10:09 -0300

    Adicionado Desafio 14

---
+++ Desafio_14/ExcluirNome.py
@@ -0,0 +1,25 @@
+nomes_possiveis = [
+    "Ana", "Carlos", "Beatriz", "Daniel", "Eduardo", "Fernanda",
+    "Gabriel", "Helena", "Luiz", "Mariana", "Luiz", "Sofia",
+    "Larissa", "Isabela", "Ricardo", "Julia", "Marcelo", "Larissa",
+    "Felipe", "Ana"
+]
+
+
+nova_lista_nomes = []
+
+for nome_atual in nomes_possiveis:
+        if nome_atual not in nova_lista_nomes:
+            nova_lista_nomes.append(nome_atual)
+
+for nome_excluido in nova_lista_nomes:
+    print(nome_excluido)
+
+
+
+
+
+
+
+
+
